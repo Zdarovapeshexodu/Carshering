29781645b8655afb2115a2e641909b94dbb4201b
Imja <dudko_06@inbox.ru>
2023-11-29 02:26:06 +0300

    Initial commit

---
+++ src/main/java/by/Carshering/Carshering/CarsheringApplication.java
@@ -0,0 +1,13 @@
+package by.Carshering.Carshering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsheringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarsheringApplication.class, args);
+	}
+
+}
